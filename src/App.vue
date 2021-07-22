84affa74f9de5ff592097680918a8047108efaa8
黃琮壬 <404250348@gapp.fju.edu.tw>
2021-07-23 01:23:31 +0800

    [feat]:add static file

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="row">
+      <div class="col-6 px-lg-3 text-end text-lg-start p-0">
+        <router-link to="/">
+        <picture>
+          <source
+            media="(min-width: 992px)"
+            srcset="./assets/images/logo--pc.png"
+          />
+          <img
+            to="/"
+            alt="Cathay Bank Logo"
+            height="50"
+            src="./assets/images/logo--mobile.png"
+          />
+        </picture>
+        </router-link>
+      </div>
+      <div class="col-6 px-lg-3 text-start text-lg-end p-0">
+        <router-link to="/service"><p style="padding-top: 5px;">立即購</p></router-link>
+      </div>
+    </div>
   </div>
-  <router-view/>
+  <router-view />
+  <div class="px-0 pt-3" id="footer">
+    <div class="row">
+      <div class="col-lg-1 offset-lg-2">
+        <p class="font-bold d-none d-lg-block">
+          謹&nbsp;慎&nbsp;理&nbsp;財<br />信&nbsp;用&nbsp;無&nbsp;價
+        </p>
+        <p class="d-lg-none text-center">
+          謹慎理財&nbsp;信用無價
+        </p>
+      </div>
+      <div class="col-lg-9  px-5 font-light">
+        <p class="description">
+          信用卡暨預借現金之各级別循環信用年利率為6.75%~15%（依本行信用評分制度定期評估,循環利率之基準日為104年9月1日）。<br />
+          預借現金手續費：預借現金金額乘以3%加上新臺幣150元或美元5元，其他相關費用係依本行網站公告。
+        </p>
+      </div>
+    </div>
+    <div class="copyright text-center pt-3 pb-1">
+      <p>&copy;&nbsp;國泰世華商業銀行股份有限公司</p>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;700&family=Roboto&display=swap");
+@import "~bootstrap/scss/bootstrap";
+@import "assets/scss/main";
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,89 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <div class="container-fluid p-0">
+      <picture>
+        <source
+          media="(min-width: 992px)"
+          srcset="../assets/images/banner-home--pc.png"
+        />
+        <img
+          class="img-fluid"
+          alt="Cathay Bank Home Banner"
+          src="../assets/images/banner-home--mobile.png"
+        />
+      </picture>
+    </div>
+    <div id="home-body">
+      <div class="container p-lg-5 p-3">
+        <!-- 歡迎標題 -->
+        <div class="row">
+          <div class="offset-lg-1">
+            <p class="font-bold text-start px-3 d-none d-lg-block">
+              立即購快捷支付，登錄開通好康多！
+            </p>
+            <p class="font-bold text-center px-3 d-lg-none">
+              立即購快捷支付<br />登錄開通好康多！
+            </p>
+          </div>
+        </div>
+        <!-- 流程圖 -->
+        <div class="row">
+          <!-- Step 1 -->
+          <div class="col-lg-4 text-center">
+            <div class="container">
+              <p class="font-step">Step 1</p>
+              <img
+                class="img-fluid"
+                alt="Step-1"
+                src="../assets/images/prepare-paper.png"
+              />
+              <p class="font-bold">國泰世華銀行<br />登錄立即購服務</p>
+            </div>
+          </div>
+          <!-- Step 2 -->
+          <div class="col-lg-4 text-center">
+            <div class="row">
+              <div class="col-lg-2 step-icon">
+                <img src="../assets/images/icon_p.png" />
+              </div>
+              <div class="col-lg-8">
+                <div class="container">
+                  <p class="font-step">Step 2</p>
+                  <img
+                    class="img-fluid"
+                    alt="Step-2"
+                    src="../assets/images/upload-success-pc.png"
+                  />
+                  <p class="font-bold">TreeMall 開通<br />立即購服務</p>
+                </div>
+              </div>
+              <div class="col-lg-2 step-icon">
+                <img src="../assets/images/icon_p.png" />
+              </div>
+            </div>
+          </div>
+          <!-- Step 3 -->
+          <div class="col-lg-4 text-center">
+            <div class="container">
+              <p class="font-step">Step 3</p>
+              <img
+                class="img-fluid"
+                alt="Step-3"
+                src="../assets/images/shopping-bag.png"
+              />
+              <p class="font-bold">TreeMall 開始使用<br />立即購服務</p>
+            </div>
+          </div>
+        </div>
+        <!-- 跳轉按鈕 -->
+        <div class="container-fluid text-center p-lg-5 py-3">
+          <router-link to="/service">
+            <button class="btn btn-primary px-5 rounded-pill">
+              立即購服務登錄
+            </button>
+          </router-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
